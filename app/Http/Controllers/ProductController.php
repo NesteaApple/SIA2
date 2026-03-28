97c95a02328283b490d97fbcc49f6b0e1197421e
Henre Saludo <henreaidenrysaludo@gmail.com>
2026-03-28 15:02:04 +0800

    feat: implement product MVC structure

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,17 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Models\Product; // Add this import
+use App\Models\Product; 
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index() {
-        // Fetch all products from DB
-        $products = Product::all(); 
-        
-        // Return view and pass the products data
-        return view('products.index', compact('products'));
+      $products = \App\Models\Product::all();
+      
+      return view('products.index', compact('products'));
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,5 @@
 <h1>Product List</h1>
 
-@if($products->isEmpty())
-    <p>No products found. (Try adding some in your database!)</p>
-@else
-    @foreach($products as $product)
-        <p>{{ $product->name }} - {{ number_format($product->price, 2) }} (Qty: {{ $product->quantity }})</p>
-    @endforeach
-@endif
+@foreach($products as $product)
+    <p>{{ $product->name }} - {{ $product->price }}</p>
+@endforeach
